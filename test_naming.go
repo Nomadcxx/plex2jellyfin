414fa8123f077a916ec02f55d39924d5234106e3
Nomadcxx <noovie@gmail.com>
2026-01-25 23:53:37 +1100

    fix: update all IsTVEpisodeFromPath callers with hint parameter

--- test_naming.go
+++ test_naming.go
@@ -8,13 +8,13 @@ import (
 func main() {
 	filename := "FAST.&.FURIOUS.2009.1080p.CatchPlay.WEB-DL.H.264.AAC-HHWEB.mkv"
 	path := "/mnt/NVME3/Sabnzbd/complete/movies/FAST.&amp;amp;.FURIOUS.2009.1080p.CatchPlay.WEB-DL.H.264.AAC-HHWEB/" + filename
-	
+
 	fmt.Printf("Filename: %s\n", filename)
 	fmt.Printf("IsMovieFilename: %v\n", naming.IsMovieFilename(filename))
 	fmt.Printf("IsTVEpisodeFilename: %v\n", naming.IsTVEpisodeFilename(filename))
-	fmt.Printf("IsTVEpisodeFromPath: %v\n", naming.IsTVEpisodeFromPath(path))
-	fmt.Printf("IsMovieFromPath: %v\n", naming.IsMovieFromPath(path))
-	
+	fmt.Printf("IsTVEpisodeFromPath: %v\n", naming.IsTVEpisodeFromPath(path, naming.SourceUnknown))
+	fmt.Printf("IsMovieFromPath: %v\n", naming.IsMovieFromPath(path, naming.SourceUnknown))
+
 	if info, err := naming.ParseMovieName(filename); err == nil {
 		fmt.Printf("Movie parse: Title=%q Year=%q\n", info.Title, info.Year)
 	} else {
