12d1edb92317e0e3317e094c33a2ff3f6dc8a615
Nomadcxx <noovie@gmail.com>
2026-01-26 12:50:38 +1100

    refactor(cli): create shared formatter utility

--- cmd/jellywatch/main.go
+++ cmd/jellywatch/main.go
@@ -980,16 +980,3 @@ func isMediaFileCheck(ext string) bool {
 	}
 	return mediaExts[ext]
 }
-
-func formatBytes(bytes int64) string {
-	const unit = 1024
-	if bytes < unit {
-		return fmt.Sprintf("%d B", bytes)
-	}
-	div, exp := int64(unit), 0
-	for n := bytes / unit; n >= unit; n /= unit {
-		div *= unit
-		exp++
-	}
-	return fmt.Sprintf("%.1f %cB", float64(bytes)/float64(div), "KMGTPE"[exp])
-}
